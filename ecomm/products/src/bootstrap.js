bc35190dbd99caf5464a5fd33bcc5514cdcd9ff8
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-24 08:10:28 -0800

    3.27 Refactoring Products - Add mount function to control running the app in isolation and in container

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,20 +1,37 @@
 import faker from 'faker';
-let products = '';
-for (let i = 0; i < 3; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
 
-document.querySelector('#dev-products').innerHTML = products;
+const mount = (el) => {
+    let products = '';
+    
+    for (let i = 0; i < 3; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+
+    el.innerHTML = products;
+};
 
 // Context/Situation #1
 // We are running this file in development in isolation
 // We are using local index.html file, which DEFINETELY 
-// has an element with id of '#dev-products'.
+// has an element with id of 'dev-products'.
 // We want to immediately render our app into that element
+
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    //Assuming our container does not have an element with 
+    //id 'dev-products'
+    if (el) {
+        // We are probably running in isolation (from container)
+        mount(el);
+    }
+}
 
 // Context/Situation #2
 // We are running this file in development or production
 // through CONTAINER app and there is NO GUARANTEE that
-// '#dev-products' exists. 
+// 'dev-products' exists. 
 // WE DO NOT WANT try to immediately render the app.
+
+export { mount };
