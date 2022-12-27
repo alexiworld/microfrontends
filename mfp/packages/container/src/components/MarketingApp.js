665462609c32fbae6db9894e6063012583792bcd
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-26 17:22:40 -0800

    5.46 Generic Integration

---
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -0,0 +1,18 @@
+import { mount } from 'marketing/MarketingApp';
+import React, {useRef, useEffect } from 'react';
+
+export default () => {
+    // The approach with useRef, useEffect, mount can
+    // be used with any other framework as long that
+    // framework can render itself in some HTML element.
+
+    const ref = useRef(null);
+
+    // useEffect ensures the code is executed only once, 
+    // when the component shows up.
+    useEffect(() => {
+        mount(ref.current);
+    });
+
+    return <div ref={ref} />;
+}
