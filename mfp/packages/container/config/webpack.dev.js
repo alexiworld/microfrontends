355d51355298e40dc81e698226f98f45fe72b398
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-26 17:49:59 -0800

    5.48 Delegating Shared Module Selection - using package.json's dependencies as shared libraries for container and marketing

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
 const commonConfig = require('./webpack.common'); 
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -18,7 +18,9 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared: ['react', 'react-dom'],
+            // package.json's dependencies are what will be used in production 
+            // and probably shared between the modules.
+            shared: packageJson.dependencies,
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
