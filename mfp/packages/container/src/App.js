8e4c45f430cce5cd3ff4564c5f2cb1d54ce4b6a2
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-29 14:49:32 -0800

    Section 9: Handling CSS in Microfrontends, 9.78 Header Component, 9.79 Adding a Header

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-
+import Header from './components/Header';
 // mount was moved to ./components/MarketingApp
 //import { mount } from 'marketing/MarketingApp'
 //console.log(mount);
 
 export default () => {
-    return <div>
-        <h1>Hi there!</h1>
-        <hr/>
-        <MarketingApp/>
-        </div>;
+    // Note placing <BrowserRouter></BrowserRouter> in
+    // (); is very important to prevent you from seeing 
+    // some weird export error in the browser console.
+    return (
+        <BrowserRouter>
+        <div>
+            <Header/>
+            <MarketingApp/>
+        </div>
+        </BrowserRouter>
+    );
 };
