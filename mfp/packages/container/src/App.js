9db4db85725bc23b6a71aa146e3e7b556ea989e2
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-30 06:06:45 -0800

    9.85 Verifying the Fix to PROD issue with colliding CSS class names (e.g. jss1, jss2, ..)

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-// mount was moved to ./components/MarketingApp
-//import { mount } from 'marketing/MarketingApp'
-//console.log(mount);
+
+// To make material-ui generate truly randombly styles for
+// different app. Prior to this the problem in production
+// was that container's random class names were jss1, jss2, 
+// ... , which colided with marketing's random class names
+// being the same. The browser was combining the styles
+// properties defined in each one of the microfrontend apps
+// and the final result was impacting each one of them as
+// marketing properties have been applied to container and
+// vice versa. By introducing a different (custom) prefix
+// for each one of the microfrontend apps we solve the 
+// production problem. Note that this problem does not exist
+// in development, where meaningful but very long names (not
+// short names) are used.
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     // Note placing <BrowserRouter></BrowserRouter> in
     // (); is very important to prevent you from seeing 
     // some weird export error in the browser console.
     return (
-        <BrowserRouter>
-        <div>
-            <Header/>
-            <MarketingApp/>
-        </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp/>
+            </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
--- mfp/packages/marketing/src/App.js
+++ mfp/packages/marketing/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// To make material-ui generate truly randombly styles for
+// different app. Prior to this the problem in production
+// was that container's random class names were jss1, jss2, 
+// ... , which colided with marketing's random class names
+// being the same. The browser was combining the styles
+// properties defined in each one of the microfrontend apps
+// and the final result was impacting each one of them as
+// marketing properties have been applied to container and
+// vice versa. By introducing a different (custom) prefix
+// for each one of the microfrontend apps we solve the 
+// production problem. Note that this problem does not exist
+// in development, where meaningful but very long names (not
+// short names) are used.
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
+
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing}/>
