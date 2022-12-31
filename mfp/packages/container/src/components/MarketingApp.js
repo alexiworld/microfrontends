f227a294c0669f644a79539acae1c028a8719bce
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-30 16:33:16 -0800

    10.95 Syncing History Objects - update container's browser history with the pathname from marketing app. Fix the potential inifinite loop in sending events between marketing app and the container

--- mfp/packages/container/src/components/MarketingApp.js
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -19,7 +19,16 @@ export default () => {
             // rename it to nextPath. {pathname} is to
             // project, and :nextPathname is to rename it.
             onNavigate: ({pathname: nextPathname}) => {
-                history.push(nextPathname);
+                // To prevent infinite loop when marketing notifies
+                // the container and updates browser history, and 
+                // that one causes to push event from the container
+                // to the marketing app to update marketing app memory
+                // history, which triggers event to the container, and
+                // so on. The following line and "if" condition solves it.
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
             }
         });
     });
