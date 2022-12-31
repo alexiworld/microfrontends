eaedebbc9a2128f853e0b5cd2df3fafd4ebeadf9
Alexi Jordanov <alexiworld@yahoo.com>
2022-12-31 11:16:26 -0800

    11.102 A Deeper Dive on Public Path. changed marketing to auth in one of the comments, nothing special

--- mfp/packages/auth/config/webpack.prod.js
+++ mfp/packages/auth/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name][contenthash].js',
-        // Settting publicPath so marketing's remoteEntry.js 
+        // Settting publicPath so auth's remoteEntry.js 
         // can successfully reference other files.
         publicPath: '/auth/latest/'
     },
