e9e230cf7ff6e82c56020c1c438db4d37bcddbe0
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-01 10:42:00 -0800

    11.106 Lazily Loading SubApps. Notice that Marketing appears to load almost immediately, clear the network tab, and click on the Login button, and you will see AuthApp files being loaded right after. To see that even Marketing is loaded lazy, in the network tab of the browser's development tools change 'No throttling' to 'Slow 3G' in the dropdown list next to Disable cache checkbox. Refresh the home page and you will see Loading... being shown first and then replaced by the Marketing app

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { lazy, Suspense  } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() =>  import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 // To make material-ui generate truly randombly styles for
 // different app. Prior to this the problem in production
@@ -31,10 +32,12 @@ export default () => {
             <BrowserRouter>
             <div>
                 <Header/>
-                <Switch>
-                    <Route path="/auth" component={AuthApp}/>
-                    <Route path="/" component={MarketingApp}/>
-                </Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <Route path="/auth" component={AuthLazy}/>
+                        <Route path="/" component={MarketingLazy}/>
+                    </Switch>
+                </Suspense>
             </div>
             </BrowserRouter>
         </StylesProvider>
